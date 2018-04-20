7dcb823ec29643b409cd7a6369e1015db4471ad1
William Derksen <wderksen98@gmail.com>
2018-04-19 22:12:50 -0400

    finished barn repair

--- greedy/src/BarnRepair.java
+++ greedy/src/BarnRepair.java
@@ -1,6 +1,24 @@
+import java.util.*;
+
 public class BarnRepair {
     public static int solve(int M, int[] occupied) {
-        // TODO
-        return 0;
+        Arrays.sort(occupied);
+        PriorityQueue<Integer> SpaceRegions = new PriorityQueue<>();
+
+        int prev = occupied[0];
+        for (int i : occupied){
+            if (Math.abs(i - prev) <= 1){
+            }else{
+                SpaceRegions.add(i-prev-1);
+            }
+            prev = i;
+        }
+        int takenspaces = occupied.length;
+
+        while (SpaceRegions.size() > (M-1)){//(int i = 0; i < SpaceRegions.size() - (M-1); i++){
+            takenspaces += SpaceRegions.poll();
+        }
+
+        return takenspaces;
     }
 }
